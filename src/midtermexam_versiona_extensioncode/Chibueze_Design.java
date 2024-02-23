8b0aafe77fb0e9b33ac6f9f2b32951a470376aa6
ekech <ekech@10.16.8.156>
2024-02-23 16:17:25 -0500

    Branch

---
+++ src/midtermexam_versiona_extensioncode/Chibueze_Design.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author ekech
+ */
+public class Chibueze_Design {
+//    My favourite subject is Math
+//    I love math because its tasking and straightforward
+}
